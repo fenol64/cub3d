46435a58c3e306053cbf04c6ccb7c39a4f3fd8b4
Fernando Nascimento Oliveira <fnascime@c2r6s7.42.rio>
2024-06-06 20:41:59 -0300

    commit

--- src/handlers/handle_map.c
+++ src/handlers/handle_map.c
@@ -6,7 +6,7 @@
 /*   By: fnascime <fnascime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 18:49:21 by fnascime          #+#    #+#             */
-/*   Updated: 2024/05/31 19:59:48 by fnascime         ###   ########.fr       */
+/*   Updated: 2024/06/06 20:41:46 by fnascime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@ t_bool    handle_map(char *path, t_cube *cube)
 {
     (void)path;
     (void)cube;
+    ft_printf("to no map handler")
     return (FALSE);
 }
