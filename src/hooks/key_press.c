2a0da3ea4d4d240a6e74e32b2190942cf5df7877
Fernando Nascimento Oliveira <fnascime@c2r9s6.42.rio>
2024-07-10 09:37:01 -0300

    compile

--- src/hooks/key_press.c
+++ src/hooks/key_press.c
@@ -6,7 +6,7 @@
 /*   By: fnascime <fnascime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 03:25:11 by fenol             #+#    #+#             */
-/*   Updated: 2024/07/10 07:06:41 by fnascime         ###   ########.fr       */
+/*   Updated: 2024/07/10 09:34:13 by fnascime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int	key_press_hook(int keycode, t_cube *cube)
 {
+	(void) cube;
 	if (keycode == KEY_ESC)
 		exit(0);
 	return (0);
